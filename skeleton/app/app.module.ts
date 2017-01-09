b7dc61348479850057f1a3ec98ca234311fcb4c0
smtandabuzo <sazi.mtandabuzo@metropolitan.co.za>
2017-01-09 11:48:09 +0200

    Adding the core module

--- skeleton/app/app.module.ts
+++ skeleton/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BugModule } from './bugs/bug.module';
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
 
 //Component
 import{ AppComponent } from './app.component';
 import { NavBarComponent } from './navbar/navbar.component';
 
 @NgModule({
-    imports: [ BrowserModule, BugModule, AppRoutingModule ],
+    imports: [ BrowserModule, BugModule, AppRoutingModule, CoreModule.forRoot() ],
     declarations: [ AppComponent,NavBarComponent ],
     bootstrap: [ AppComponent ]
 })
---
+++ skeleton/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule, ModuleWithProviders, Optional, SkipSelf } from '@angular/core';
+
+@NgModule({
+    imports: [],
+    declarations: [],
+    exports: []
+})
+export class CoreModule{
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule){
+        if(parentModule){
+            throw new Error("CoreModule already exists. Only import in the root/app module");
+        }
+    }
+
+    static forRoot(): ModuleWithProviders{
+        return{
+            ngModule: CoreModule,
+            providers: []
+        };
+    }
+}
